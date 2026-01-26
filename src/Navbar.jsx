a764216b89bbddd0f387c99b8fca1ba25d8995bf
ArshilTech <arshilmasood@gmail.com>
2026-01-26 20:32:17 +0530

    feat: initialize React app with Tailwind CSS, create main components
    - Added App component to serve as the main application container.
    - Created Navbar component for navigation with links and a menu button.
    - Implemented Hero component as a placeholder for future content.
    - Set up index.css to import Tailwind CSS styles.
    - Configured Vite for React and Tailwind CSS integration.

---
+++ src/Navbar.jsx
@@ -0,0 +1,22 @@
+
+function Navbar(){
+
+    return(
+        <div>
+            <div className="flex justify-between border-2 py-1 px-2">
+                <h2 className="text-lg font-mono">IU GPA Calculator</h2>
+                <div className="flex gap-4">
+                    <a href="#" className="text-lg font-mono">Sources</a>
+                    <a href="#" className="text-lg font-mono">About Me</a>
+                    <button aria-label="Open menu" className="flex flex-col justify-center gap-1.5 cursor-pointer">
+                        <span className="h-0.5 w-6 bg-slate-800 rounded"></span>
+                        <span className="h-0.5 w-6 bg-slate-800 rounded"></span>
+                        <span className="h-0.5 w-6 bg-slate-800 rounded"></span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
