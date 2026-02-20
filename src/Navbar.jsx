b1edc030e7fb71c2095299d0317528df13ec55e4
ArshilTech <arshilmasood@gmail.com>
2026-02-20 22:56:32 +0530

    style: update About Me link to point to LinkedIn profile

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar(){
                 <h2 className="text-2xl font-mono text-[#F2B50B]">IU GPA Calculator</h2>
                 <div className="flex gap-6">
                     <a href="/Sources.html" className="text-2xl font-mono text-[#F2B50B]  hover:text-white">Sources</a>
-                    <a href="#" className="text-2xl font-mono text-[#F2B50B] hover:text-white">About Me</a>
+                    <a href="https://www.linkedin.com/in/arshil-masood-061386308" className="text-2xl font-mono text-[#F2B50B] hover:text-white">About Me</a>
                     <button aria-label="Open menu" className="flex flex-col justify-center gap-1.5 cursor-pointer text-2xl" onClick={dropDown}>
                         <span className="h-0.5 w-6 bg-[#F7F0F0] rounded"></span>
                         <span className="h-0.5 w-6 bg-[#F7F0F0] rounded"></span>
