7b87f4f9b14faec1435e046a45b734faa54bf13f
ArshilTech <arshilmasood@gmail.com>
2026-02-20 22:17:37 +0530

    style: update Navbar link to point to Sources.html and add Sources.html file

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar(){
             <div className="flex justify-between border-2 py-1 px-2 bg-[#48A111]">
                 <h2 className="text-2xl font-mono text-[#F2B50B]">IU GPA Calculator</h2>
                 <div className="flex gap-6">
-                    <a href="#" className="text-2xl font-mono text-[#F2B50B]  hover:text-white">Sources</a>
+                    <a href="/Sources.html" className="text-2xl font-mono text-[#F2B50B]  hover:text-white">Sources</a>
                     <a href="#" className="text-2xl font-mono text-[#F2B50B] hover:text-white">About Me</a>
                     <button aria-label="Open menu" className="flex flex-col justify-center gap-1.5 cursor-pointer text-2xl" onClick={dropDown}>
                         <span className="h-0.5 w-6 bg-[#F7F0F0] rounded"></span>
